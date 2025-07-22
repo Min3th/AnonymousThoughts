03012b95c3929f7854208bae8e06a755b4c69675
Min3th <minethweerasinghe@gmail.com>
2025-07-22 22:54:35 +0530

    fix connection with mongo db.

--- thoughts-backend/index.js
+++ thoughts-backend/index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require("express");
 const connectDB = require("./db");
-const Thought = require("./models/Thought");
+const Thought = require("./thoughts");
 const cors = require("cors");
 
 const app = express();
@@ -16,6 +16,23 @@ app.get("/api/thoughts", async (req, res) => {
   try {
     const thoughts = await Thought.find();
     res.json(thoughts);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.post("/api/thoughts", async (req, res) => {
+  try {
+    const { topic, content } = req.body;
+
+    if (!topic || !content) {
+      return res.status(400).json({ error: "Topic and content are required" });
+    }
+
+    const newThought = new Thought({ topic, content });
+    await newThought.save();
+
+    res.status(201).json(newThought);
   } catch (err) {
     res.status(500).json({ error: "Server error" });
   }
